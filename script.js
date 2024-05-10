b76bc4001637d67db832b67d9b5ad905b9b6f69d
Justin <justinereifanwijaya@gmail.com>
2024-05-10 21:47:33 +0700

    update handle error

--- script.js
+++ script.js
@@ -1,32 +1,47 @@
-
-
-// .then(res=>res.json())
-// .then(res=>console.log(res))
-
-
-const myResult = document.querySelector('#myResult');
 const convertBtn = document.querySelector('#convertBtn');
-
-
-convertBtn.addEventListener('click',function(){
-  const inputAngka = document.querySelector('#inputAngka').value;
-  console.log(inputAngka);
+convertBtn.addEventListener('click',async function(){
   
-  if(inputAngka){
-    const currentKurs = document.querySelector('#curKurs').value;
-    const newKurs = document.querySelector('#newKurs').value;
-    const url = 'https://api.api-ninjas.com/v1/convertcurrency?want='+newKurs+'&have='+currentKurs+'&amount='+inputAngka;
-    fetch(url,{
+    try{
+      const inputAngka = document.querySelector('#inputAngka').value;
+      const currentKurs = document.querySelector('#curKurs').value;
+      const newKurs = document.querySelector('#newKurs').value;
+      const data = await getData(currentKurs,newKurs,inputAngka);
+      updateUI(data);
+    }
+    catch(e){
+      updateUI(e);
+    }
+})
+
+function getData(curData,newData,inputAngka){
+  
+    const url = 'https://api.api-ninjas.com/v1/convertcurrency?want='+curData+'&have='+newData+'&amount='+inputAngka;
+    return fetch(url,{
       method: 'GET',
       headers: {
         'X-Api-Key': 'Q7dm36l7FggKUvvEDtutwkvgEcGnFKMQHeUpcdVg',
       },
     })
-    .then(e=>e.json())
     .then(e=>{
-      myResult.value = Math.round(e.new_amount);
-      console.log(e);
+      if(!inputAngka){
+        throw "Input angka terlebih dahulu!"
+      }
+      if(curData == 'Kurs' || newData == 'Kurs'){
+        throw "Pilih kurs terlebih dahulu!"
+      }
+      if(!e.ok){
+        throw "API KEY salah!";
+      }
+      return e.json();
     })
+    .then(e=>{
+      return Math.round(e.new_amount);
+    })
+    
+}
 
-  }
-})
+
+function updateUI(resultText){
+  const myResult = document.querySelector('#myResult');
+  myResult.value = resultText;
+}
